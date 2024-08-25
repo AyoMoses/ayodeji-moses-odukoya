d8dc4216ace321cb58d7025e231db28fe7d58837
ayomoses <odukoyaayodeji@gmail.com>
2024-08-25 16:54:50 +0100

    header and footer components added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,13 @@
 import React from 'react';
 import { createClient } from '@/prismicio';
-import Link from 'next/link';
-import { PrismicNextLink } from '@prismicio/next';
+import NavBar from '@/components/NavBar';
 
 export default async function Header() {
   const client = createClient();
   const settings = await client.getSingle('settings');
   return (
     <header className='top-0 z-50 mx-auto max-w-7xl md:sticky md:top-4'>
-      <nav>
-        <ul>
-          <li>
-            <Link href='/' aria-label='Home page'>
-              {settings.data.name}
-            </Link>
-          </li>
-          {settings.data.nav_item.map(({ item_link, item_label }, index) => (
-            <li key={index}>
-              <PrismicNextLink field={item_link}>{item_label}</PrismicNextLink>
-            </li>
-          ))}
-        </ul>
-      </nav>
+      <NavBar settings={settings} />
     </header>
   );
 }
