4289809544420f0cf4a95107d61d40ab049eb1b1
ayomoses <odukoyaayodeji@gmail.com>
2024-08-25 17:16:00 +0100

    adding noise bg to homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Urbanist } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import clsx from 'clsx';
 
 const font = Urbanist({ subsets: ['latin'] });
 
@@ -18,9 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en' className='bg-[#1c2541] text-slate-100'>
-      <body className={font.className}>
+      <body className={clsx(font.className, 'relative min-h-screen')}>
         <Header />
         {children}
+        <div className='background-gradient absolute inset-0 -z-50 max-h-screen' />
+        <div className="pointer-events-none absolute inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] opacity-20 mix-blend-soft-light"></div>
         <Footer />
       </body>
     </html>
